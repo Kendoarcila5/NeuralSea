0f3c6732c5b1e1c7566f15dce839d06fdc0ea5b2
Kendoarcila5 <105925573+Kendoarcila5@users.noreply.github.com>
2026-02-28 04:14:41 -0600

    Add Mint component for NFT creation

---
+++ pages/mint.js
@@ -0,0 +1,8 @@
+export default function Mint() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Mint NFT</h1>
+      <p>Funcionalidad de crear NFT próximamente</p>
+    </div>
+  );
+}

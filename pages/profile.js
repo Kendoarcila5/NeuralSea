dad3df2c23f302ab2772f0bd8483189647c70ac8
Kendoarcila5 <105925573+Kendoarcila5@users.noreply.github.com>
2026-02-28 04:56:00 -0600

    Create Profile component for user NFTs display

---
+++ pages/profile.js
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Mi Perfil</h1>
+      <p>Lista de NFTs del usuario</p>
+    </div>
+  );
+}
